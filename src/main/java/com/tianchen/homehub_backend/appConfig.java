2f591bf45a5b9ac83a649845d22c4e9816252a75
Tianchen zhang <jasontc196@gmail.com>
2024-09-17 03:32:40 -0400

    添加了websocket 相关的网络配置

--- src/main/java/com/tianchen/homehub_backend/appConfig.java
+++ src/main/java/com/tianchen/homehub_backend/appConfig.java
@@ -2,14 +2,19 @@ package com.tianchen.homehub_backend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.tianchen.homehub_backend.Service.UserService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 
 import java.util.Arrays;
@@ -21,8 +26,10 @@ public class appConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors()
+                .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/register","/api/login","/api/logout","/api/username").permitAll() // 允许未认证用户访问
+                .requestMatchers("/api/register","/api/login","/api/logout","/api/username","/chat/**").permitAll() // 允许未认证用户访问
                 .anyRequest().authenticated() // 其他请求都需要认证
                 .and()
                 .formLogin() // 启用基于表单的登录页面
@@ -34,17 +41,26 @@ public class appConfig {
         return http.build();
     }
 
+
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
                         .maxAge(3600);
+//                registry.addMapping("/chat/**")
+//                        .allowedOrigins("http://localhost:3000")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true)
+//                        .maxAge(3600);
+
             }
         };
     }
